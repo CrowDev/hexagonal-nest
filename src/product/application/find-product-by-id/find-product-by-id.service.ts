84684339fbc0e5d41470f72b81e1a42fe4171f3c
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-21 05:00:24 -0300

    connecting app with output db port

--- src/product/application/find-product-by-id/find-product-by-id.service.ts
+++ src/product/application/find-product-by-id/find-product-by-id.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ProductRepository } from 'src/product/adapters/db/product.repository';
 import { IFindProductByIdUseCase } from 'src/product/domain/use-cases/use-cases.service.interface';
 
 @Injectable()
 export class FindProductByIdService implements IFindProductByIdUseCase {
+  constructor(private productRepository: ProductRepository) {}
   async findProductById(id: number): Promise<any> {
+    return await this.productRepository.findProductById(id);
   }
 }
