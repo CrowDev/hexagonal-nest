42e48f9abb678d7585ecfa1374a1b669e2f9ada0
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-21 05:17:27 -0300

    connected app with db adapter through db port

--- src/product/adapters/db/product.repository.ts
+++ src/product/adapters/db/product.repository.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { IProductRepository } from 'src/product/domain/ports/db.repository.interface';
+import { Repository } from 'typeorm';
+import { ProductEntity } from './entities/product.entity';
 
 @Injectable()
 export class ProductRepository implements IProductRepository {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private repository: Repository<ProductEntity>,
+  ) {}
+
   async findProductById(id: number): Promise<any> {
+    return await this.repository.findOneBy({ id });
   }
 
   async findAllProducts(): Promise<any> {
