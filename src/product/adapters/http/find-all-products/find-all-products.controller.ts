57a23387da8334dc16cb5a7f4ba3f488cfeb4a3e
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-21 09:37:46 -0300

    connect http adapter with app flow

--- src/product/adapters/http/find-all-products/find-all-products.controller.ts
+++ src/product/adapters/http/find-all-products/find-all-products.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { FindAllProductsService } from 'src/product/application/find-all-products/find-all-products.service';
 import { IFindAllProducts } from 'src/product/domain/ports/http.controller.interface';
 
 @Controller('find-all-products')
 export class FindAllProductsController implements IFindAllProducts {
+  constructor(private findAllProductsService: FindAllProductsService) {}
   @Get()
-  findAllProducts(): Promise<any> {
+  async findAllProducts(): Promise<any> {
+    return await this.findAllProductsService.findAllProducts();
   }
 }
