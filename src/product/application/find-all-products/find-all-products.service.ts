cb7e982f012e680267fbd63f29de4c951e3a39e9
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-21 09:31:44 -0300

    connect to db adapter to get all products

--- src/product/application/find-all-products/find-all-products.service.ts
+++ src/product/application/find-all-products/find-all-products.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ProductRepository } from 'src/product/adapters/db/product.repository';
 import { IFindAllProductsUseCase } from 'src/product/domain/use-cases/use-cases.service.interface';
 
 @Injectable()
 export class FindAllProductsService implements IFindAllProductsUseCase {
+  constructor(private productRepository: ProductRepository) {}
   async findAllProducts(): Promise<any> {
+    return await this.productRepository.findAllProducts();
   }
 }
