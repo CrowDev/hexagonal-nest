283c8e4a82a4073492bef52863a076455d290def
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-21 04:45:25 -0300

    updated adapter to connect to app using port definition

--- src/product/adapters/http/find-product-by-id/find-product-by-id.controller.ts
+++ src/product/adapters/http/find-product-by-id/find-product-by-id.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { FindProductByIdService } from 'src/product/application/find-product-by-id/find-product-by-id.service';
 import { IFindProductById } from 'src/product/domain/ports/http.controller.interface';
 
 @Controller('find-product-by-id')
 export class FindProductByIdController implements IFindProductById {
+  constructor(private findProductByIdService: FindProductByIdService) {}
   @Get(':id')
   async findProductById(@Param() id: number): Promise<any> {
+    return await this.findProductByIdService.findProductById(id);
   }
 }
